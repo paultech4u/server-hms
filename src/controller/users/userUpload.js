e24eba17c595192d2fa019c1d062466172fce95f
donsimon <speak2donsimon@gmail.com>
2020-12-14 17:13:01 +0100

    hosted a cloudinary file uploads

--- src/controller/users/userUpload.js
+++ src/controller/users/userUpload.js
@@ -18,12 +18,13 @@ export const UploadProfilePicture = async function (req, res, next) {
       return result;
     };
     const { result } = await uploader(imageUrl.path);
+    // Remove saved image URL from uploads
     fs.unlinkSync(imageUrl.path);
-    user.imageUrl = result.url;
+    user.imageUrl = result.url; // TODO assigned cloudinary url to user imageUrl path
     const [u] = await Promise.all([user.save()]);
     if (!u) {
       ErrorException(404, "Upload not successful");
-    }
+    } 
     res.status(200).json({
       message: "Successfully uploaded profile picture",
     });
