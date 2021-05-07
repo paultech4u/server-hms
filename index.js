aeed979f900652fb7ae89eef6156ef88170c2744
paultech4u <speak2donsimon@gmail.com>
2021-05-07 21:44:28 +0100

    fixed module error

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
-import DatabaseConfig from './app/database/config';
+import DatabaseConfig from './database/config';
 
 import USER_ROUTE from './controller/user/api';
 import PATIENTS_ROUTE from './controller/patient/api';
